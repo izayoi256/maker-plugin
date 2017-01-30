bf36606c8fb3631db95c9628788da084eda7472e
Hama <hama@daisy.link>
2017-01-30 16:23:56 +0900

    Revert "ライセンス表記を削除"
    This reverts commit 56c3b2c5a48292f8be9ac11af25746af111b5fca.

--- Resource/doctrine/migration/Version201511201300.php
+++ Resource/doctrine/migration/Version201511201300.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of the ProductExternalLink plugin
  *
- *
+ * Copyright (C) 2017 Shotaro HAMA All Rights Reserved.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
